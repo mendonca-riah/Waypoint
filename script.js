332a13ba41a5c396d86363d842c8ef1d8b397b6b
mendonca-riah <riahannamendonca@gmail.com>
2026-09-07 14:11:16 +0530

    display user name on home page

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const createAccountButton = document.getElementById("create-account");
 
+if (createAccountButton) {
 createAccountButton.addEventListener("click", function () {
 
     const username = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    console.log(username);
-    console.log(email);
-    console.log(password);
+    if (username === "" || email === "" || password === "") {
+        alert("Please fill in all the fields.");
+        return;
+    }
 
+    localStorage.setItem("userName", username);
+
+    alert("Your account has been created!");
+
+    window.location.href = "index.html";
 });
+}
+
+const userName = localStorage.getItem("userName");
+
+const userNameElement = document.getElementById("user-name");
+
+if (userNameElement) {
+    userNameElement.textContent = userName;
+}
